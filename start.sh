86f86721cfb7049ffd0f43a0bea52fef0b36197c
rilesc555 <109360663+rilesc555@users.noreply.github.com>
2024-06-14 11:44:15 -0500

    add start script

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Change directory to /src/razor-imu-ros2-humble/
+cd /src/razor-imu-ros2-humble/
+
+# Use rosdep to install dependencies
+rosdep install --from-paths razor_imu_ros2/ --ignore-src -y -r
